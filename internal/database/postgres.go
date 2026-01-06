0bc9e786192583d98cc916d12cb7b9470ea91bd3
Yusuf Yonturk <yusufyonturk.kisisel@gmail.com>
2026-01-06 09:45:00 +0300

    feat: postgresql bağlantı havuzunun oluşturulması

---
+++ internal/database/postgres.go
@@ -0,0 +1,21 @@
+package database
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/lib/pq"
+)
+
+func Connect(url string) (*sql.DB, error) {
+	db, err := sql.Open("postgres", url)
+	if err != nil {
+		return nil, fmt.Errorf("failed to open db connection: %v", err)
+	}
+
+	if err := db.Ping(); err != nil {
+		return nil, fmt.Errorf("failed to ping db: %v", err)
+	}
+
+	return db, nil
+}
